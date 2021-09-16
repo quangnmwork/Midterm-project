ee3411efa153ea345d2c3d8eefb1b10e050327c9
Nguyen Minh Quang <quangnm.work@gmail.com>
2021-09-16 15:01:54 +1400

    16/9

--- loginView.js
+++ loginView.js
@@ -41,7 +41,7 @@ const loginUserHandler = () => {
     btnLoginUser.addEventListener('click', (event) => {
         event.preventDefault() ; 
         errorLoginHandler();
-        window.location.href="/user.html"
+        window.location.pathname = '/user.html'
     })
 }
 const loginAdminHandler = () => {
